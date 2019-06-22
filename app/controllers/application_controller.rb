103c43485bebe0d54af135d64b0f4e255d1f499d
Gromyko Mikhail <gro-mikhail@yandex.ru>
2019-06-22 12:20:32 +0300

    add devise for workers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,26 @@
 class ApplicationController < ActionController::Base
-	before_action :configure_permitted_parameters_for_sign_up, if: :devise_controller?
-	before_action :configure_permitted_parameters_for_update, if: :devise_controller?
+	before_action :configure_permitted_parameters_sign_up_for_customers, if: :devise_controller?
+	before_action :configure_permitted_parameters_update_for_customers, if: :devise_controller?
+	before_action :configure_permitted_parameters_sign_up_for_workers, if: :devise_controller?
+	before_action :configure_permitted_parameters_update_for_workers, if: :devise_controller?
 
 
   	protected
 
-  	def configure_permitted_parameters_for_sign_up
+  	def configure_permitted_parameters_sign_up_for_customers
     	devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone_number])
   	end
 
-  	def configure_permitted_parameters_for_update
+  	def configure_permitted_parameters_update_for_customers
     	devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phone_number])
+  	end
+
+  	def configure_permitted_parameters_sign_up_for_workers
+    	devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone_number, :about])
+  	end
+
+  	def configure_permitted_parameters_update_for_workers
+    	devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phone_number, :about])
   	end
 
 
