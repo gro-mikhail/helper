5a1e74741dd80865cba2b3e43bd316466835b875
Gromyko Mikhail <gro-mikhail@yandex.ru>
2019-06-26 23:25:47 +0300

    delete orders

--- app/views/orders/_order.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! order, :id, :title, :description, :address, :price, :created_at, :updated_at
-json.url order_url(order, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :workers
   resources :workers
   root to: "pages#home"	
-  devise_for :customers
-  resources :customers
+  devise_for :customers 
+  resources :customers 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
